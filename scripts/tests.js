ef5a95d90e9ad1e1af48ed7b3c4ebbf0c8e5ae66
fa473 <home@homes-MacBook-Pro.local>
2017-03-08 16:21:23 -0800

    atom beautify

--- scripts/tests.js
+++ scripts/tests.js
@@ -4,16 +4,33 @@ QUnit.test('add', function(assert) {
         'james@bond.com': 'eshpresso',
         'm@bond.com': 'tea'
     };
-    var obj2 = { 'm@bond.com': 'tea' };
+    var obj2 = {
+        'm@bond.com': 'tea'
+    };
 
     assert.strictEqual(ds.add('m@bond.com', 'tea'), undefined, 'Added order');
     assert.strictEqual(ds.add('james@bond.com', 'eshpresso'), undefined, 'Added order');
     assert.deepEqual(ds.getAll(), obj);
 
-    assert.strictEqual(ds.remove('james@bond.com'),undefined, 'removed');
+    assert.strictEqual(ds.remove('james@bond.com'), undefined, 'removed');
     assert.deepEqual(ds.getAll(), obj2);
 
     assert.strictEqual(ds.get('m@bond.com'), 'tea', 'tea');
     assert.strictEqual(ds.get('james@bond.com'), undefined);
 
+});
+
+QUnit.test('truck', function(assert) {
+    (function(window) {
+        'use strict';
+        var App = window.App;
+        var Truck = App.Truck;
+        var DataStore = App.DataStore;
+        var myTruck = new Truck('ncc-1701', new DataStore());
+        window.myTruck = myTruck;
+    })(window);
+    assert.strictEqual(myTruck.createOrder({
+        email: 'me@goldfinger.com',
+        coffee: 'double mocha'
+    }), undefined);
 });
